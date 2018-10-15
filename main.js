2f417c3386bb44472ae70280c8a69a0fbc4c804a
scabbalicious <scabb99@yahoo.com>
2018-10-14 21:15:19 -0700

    add timeout for card resets

--- main.js
+++ main.js
@@ -48,6 +48,7 @@ const cardsArray = [{
 }]
 
 let gameGrid = cardsArray.concat(cardsArray) // duplicate the array to have a match for each card
+gameGrid.sort(() => 0.5-Math.random())
 
 const game = document.getElementById('game')
 const grid = document.createElement('section') // create html section
@@ -61,3 +62,57 @@ gameGrid.forEach(item => {
 	card.style.backgroundImage = `url(${item.img})` // apply background image of the div to the cardsArray image
 	grid.appendChild(card) // append div to the grid
 })
+
+grid.addEventListener('click', function(event) { //event listener for clicked item
+	let clicked = event.target
+		if (clicked.nodeName === 'SECTION' ||
+			clicked === previousTarget) { // only select div within grid
+			return
+		} if (count < 2) {
+			count++
+				if (count === 1) {
+					firstGuess = clicked.dataset.name
+					clicked.classList.add('selected')
+				} 
+				else {
+					secondGuess = clicked.dataset.name
+					clicked.classList.add('selected')
+					clicked.classList.add('selected') //add selected class
+				}
+				if (firstGuess !== '' && secondGuess !== '') {
+					if (firstGuess === secondGuess) {
+						setTimeout(match, delay) // 
+						setTimeout(resetGuesses, delay) //reset guesses upon second selection if correct after 1s delay
+					}
+					else {
+						setTimeout(resetGuesses, delay) //reset guesses upon second selection if incorrect
+					}
+				}
+				previousTarget = clicked
+			}
+
+})
+
+let firstGuess = ''
+let secondGuess = ''
+let count = 0 // counter to keep track of selected cards
+let previousTarget = null
+let delay = 1000
+
+
+const match = () => {
+	var selected = document.querySelectorAll('.selected')
+	selected.forEach(card => {
+		card.classList.add('match') // add match css
+	})
+}
+
+const resetGuesses = () => {
+	firstGuess = ''
+	secondGuess = ''
+	count = 0
+	var selected = document.querySelectorAll('.selected')
+	selected.forEach(card => {
+		card.classList.remove('selected')
+	}) //reset guesses, remove selected css
+}
